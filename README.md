37ff091f1a7ea02176ee33ac757fc728ecdfb55c
Dan Lopuch <dan@datahero.com>
2014-06-24 10:54:04 -0700

    Adding delay option

--- README.md
+++ README.md
@@ -16,5 +16,12 @@ $ npm install
 $ export HC_KEY=myhipchatapikey
 $ export HC_ROOM=1234
 $ export HC_FROM=BotName
+$ export DELAY_MS=30000
 $ npm run start
 ```
+
+Notes:
+- `HC_FROM` is optional.  Defaults to 'The Vuvuzela'.  HipChat API may puke if name is too long.
+- ESPN broadcast has been delayed by 30-60 sec compared to the FIFA feed.  Kinda sucks to get a notification of a goal
+  before you see it happen on the screen.  Use the `DELAY_MS` environment variable to delay notifications by a certain time.
+
--- ole.js
+++ ole.js
@@ -4,24 +4,39 @@ var requestify = require('requestify'),
 
 var chatroom = new HipChat(process.env.HC_KEY),
     user = process.env.HC_FROM || 'The Vuvuzela',
-    room = parseInt(process.env.HC_ROOM, 10);
+    room = parseInt(process.env.HC_ROOM, 10),
+
+    /**
+     * ESPN broadcast has been delayed by 30-60 sec compared to the FIFA feed.
+     * Kinda sucks to get a notification of a goal before you see it happen on the screen.
+     * Use the DELAY_MS environment variable to delay notifications by a certain time.
+     */
+    delayMs = process.env.DELAY_MS ? parseInt(process.env.DELAY_MS, 10) : 0;
 
 var announce = function(text) {
-  console.log('Sent to hipchat: ' + text);
+  console.log('Sent to hipchat (' + delayMs + ' delay): ' + text);
 
-  chatroom.postMessage({
-    room: room,
-    from: user,
-    message: text,
-    notify: 1,
-    color: 'yellow',
-    message_format: 'text'
-  }, function(data) {
-    if (error) {
-      console.log('  HIPCHAT API Response: ' + JSON.stringify(data));
-    }
-  });
+  setTimeout(function() {
+    chatroom.postMessage({
+      room: room,
+      from: user,
+      message: text,
+      notify: 1,
+      color: process.env.HC_COLOR || 'yellow',
+      message_format: 'text'
+    }, function(data) {
+      if (error) {
+        console.log('  HIPCHAT API Response: ' + JSON.stringify(data));
+      }
+    }),
+    delayMs
+  );
 };
+
+if (delayMs) {
+  announce("Greetings!  I'm delaying my announcements by " + Math.round(delayMs / 1000) +
+           " seconds to try to get more in sync with the broadcast");
+}
 
 bot(requestify, announce, 5000, 60000);
 
